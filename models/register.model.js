2f55e7237928bd6e9b22cc42ff8247ec3a6b2c67
RedwanAhmedTapuu <redwan25880@gmail.com>
2024-02-27 11:37:24 +0600

    rickshawmama env file updated

--- models/register.model.js
+++ models/register.model.js
@@ -5,11 +5,11 @@ const mongoose = require("mongoose");
 const userSchema = new mongoose.Schema({
   fname: {
     type: String,
-    required: true,
+   
   },
   lname: {
     type: String,
-    required: true,
+   
   },
   email: {
     type: String,
